64d78630f1e5adc199c675f93efac440db77bc7a
Josh Park <joshuapark823@gmail.com>
2021-01-10 15:55:15 -0500

    http client, json import, observable

--- data-vis/src/app/app.component.ts
+++ data-vis/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { DataService } from './services/data-service.service';
+import { HttpClient } from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'josh park';
+
+  constructor(
+    private _dataService: DataService,
+    private _http: HttpClient
+  ){}
+
+  ngOnInit() {
+    this._http.get("assets/livingcost2020.json").subscribe(data => {
+      console.log(data);
+    })
+  }
 }
--- data-vis/src/app/app.module.ts
+++ data-vis/src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { SidePanelComponent } from './side-panel/side-panel.component';
 import { DataMainComponent } from './data-main/data-main.component';
+import { DataService } from './services/data-service.service'
 
 @NgModule({
   declarations: [
@@ -14,9 +16,12 @@ import { DataMainComponent } from './data-main/data-main.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    DataService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
